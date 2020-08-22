ee4265210f2f465c3c307914185b9179cfbd27a5
somebody1234 <ehern.lee@gmail.com>
2020-08-22 22:22:29 +0959

    add filetype detection; image upload; more arguments

--- src/main.rs
+++ src/main.rs
@@ -16,26 +16,73 @@ struct Channel {
 
 #[derive(Clap)]
 struct Opts {
-   #[clap(short, long)]
+  #[clap(short, long)]
   server: Option<String>,
-   #[clap(short, long)]
+  #[clap(short, long)]
   channel: Option<String>,
+  #[clap(short, long)]
+  message: Option<String>,
+  #[clap(short, long)]
+  token: Option<String>,
+  #[clap(short, long)]
+  block: bool,
+  path: Option<String>,
+}
+
+#[cfg(feature = "filetype")]
+fn false_matcher(_buf: &[u8]) -> bool { false }
+
+#[cfg(feature = "filetype")]
+fn infer(buf: &[u8]) -> infer::Type {
+  if let Some(type_) = infer::get(&buf) {
+    type_
+  } else if buf.starts_with(b"#!/usr/bin/") || buf.starts_with(b"#!/bin/") {
+    let lang = if buf.starts_with(b"#!/usr/bin/env ") {
+      &buf[b"#!/usr/bin/env ".len()..]
+    } else if buf.starts_with(b"#!/bin/env ") {
+      &buf[b"#!/bin/env ".len()..]
+    } else if buf.starts_with(b"#!/usr/bin") {
+      &buf[b"#!/usr/bin/".len()..]
+    } else {
+      &buf[b"#!/bin/".len()..]
+    };
+    match std::str::from_utf8(&lang).expect("shebang not in utf-8").to_lowercase().as_str() {
+      "node" | "deno" | "spidermonkey" | "v8" => infer::Type::new(infer::MatcherType::APP, "application/javascript", "js", false_matcher),
+      "ts-node" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "ts", false_matcher),
+      // NOTE: i could include some minor versions; however i doubt they're used often enough, esp. in shebang lines.
+      "python" | "python2" | "python3" | "pypy" | "pypy2" | "pypy3" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "py", false_matcher),
+      "ruby" | "irb" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "rb", false_matcher),
+      "kotlinc" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "kt", false_matcher),
+      "php" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "php", false_matcher),
+      "perl" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "perl", false_matcher),
+      "raku" | "perl6" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "perl", false_matcher),
+      "lua" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "lua", false_matcher),
+
+      "sh" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "sh", false_matcher),
+      "bash" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "bash", false_matcher),
+      "zsh" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "zsh", false_matcher),
+      "fish" => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "fish", false_matcher),
+      _ => infer::Type::new(infer::MatcherType::TEXT, "text/plain", "txt", false_matcher)
+    }
+  } else {
+    infer::Type::new(infer::MatcherType::TEXT, "text/plain", "txt", false_matcher)
+  }
 }
 
 #[tokio::main]
 async fn main() -> reqwest::Result<()> {
-  let token_string = std::env::var("DISCORD_TOKEN").expect("token");
-  let token = token_string.as_str();
   let opts = Opts::parse();
+  let token_string = opts.token.unwrap_or_else(|| std::env::var("DISCORD_TOKEN").expect("missing token, please export DISCORD_TOKEN or supply command-line arg"));
+  let token = token_string.as_str();
   let mut server_name: Option<String> = opts.server.map(|s| s.to_lowercase());
   let mut channel_name: Option<String> = opts.channel.map(|s| s.to_lowercase());
   if let Ok(contents) = std::fs::read_to_string("config") {
     let parts: Vec<&str> = contents.split('\n').collect();
     if parts[0].len() > 0 {
-      server_name = Some(String::from(parts[0]).to_lowercase());
+      server_name = server_name.or(Some(parts[0].to_string().to_lowercase()));
     }
     if parts.len() > 0 && parts[1].len() > 0 {
-      channel_name = Some(String::from(parts[1]).to_lowercase());
+      channel_name = channel_name.or(Some(parts[1].to_string().to_lowercase()));
     }
   }
   let client = reqwest::Client::new();
@@ -74,15 +121,78 @@ async fn main() -> reqwest::Result<()> {
   if channel_id == None {
     panic!("invalid channel");
   } else {
-    let mut buffer = String::new();
-    if let Ok(_) = std::io::stdin().read_to_string(&mut buffer) {}
-    client.post(format!("https://discord.com/api/v6/channels/{}/messages", channel_id.unwrap()).as_str())
-      .header("authorization", token)
-      .form(&[("content", buffer.as_str())])
-      .send()
-      .await?;
+    let mut buffer = Vec::new();
+    if let Some(path) = &opts.path {
+      if let Ok(vec) = std::fs::read(path) {
+        buffer = vec;
+      }
+    } else if let Some(message) = opts.message {
+      buffer = message.as_str().as_bytes().into();
+    } else if let Ok(_) = std::io::stdin().read_to_end(&mut buffer) {
+    }
+    if buffer.len() > 0 {
+      if cfg!(feature = "filetype") {
+        let type_ = infer(&buffer);
+        let is_text = type_.matcher_type() == infer::MatcherType::TEXT || (type_.matcher_type() == infer::MatcherType::APP && type_.mime_type() == "application/javascript");
+        let mut send_file = false;
+        if opts.block {
+          if buffer.len() + 8 + type_.extension().len() > 2000 {
+            panic!("content too long; cannot put in block");
+          } else if !is_text {
+            panic!("non-text files cannot be inserted into code block");
+          }
+          let mut extension = type_.extension().to_string();
+          if let Some(path) = &opts.path {
+            extension = std::path::Path::new(path)
+              .extension().expect("filename not valid")
+              .to_str().expect("filename is not valid utf-8")
+              .to_string();
+          }
+          buffer = format!("```{}\n{}\n```", extension, std::str::from_utf8(&buffer).expect("message not utf-8")).as_bytes().into();
+        } else if is_text && buffer.len() < 2000 {
+          // text message
+        } else {
+          // file attachment; with appropriate embed if there is one
+          send_file = true;
+       
+        }
+        if send_file {
+          let mut file_part = reqwest::multipart::Part::bytes(buffer)
+            .mime_str(type_.mime_type())
+            .expect("could not set embed file mimetype");
+          let file_name = match &opts.path {
+            Some(path) => std::path::Path::new(path)
+              .file_name().expect("filename not valid")
+              .to_str().expect("filename is not valid utf-8")
+              .to_string(),
+            None => format!("message.{}", type_.extension()),
+          };
+          file_part = file_part.file_name(file_name.clone());
+          let form = reqwest::multipart::Form::new()
+            .part("file", file_part);
+          client.post(format!("https://discord.com/api/v6/channels/{}/messages", channel_id.unwrap()).as_str())
+            .header("authorization", token)
+            .multipart(form)
+            .send()
+            .await?;
+        } else {
+          client.post(format!("https://discord.com/api/v6/channels/{}/messages", channel_id.unwrap()).as_str())
+            .header("authorization", token)
+            .form(&[("content", std::str::from_utf8(&buffer).expect("message not utf-8"))])
+            .send()
+            .await?;
+        }
+      } else {
+        client.post(format!("https://discord.com/api/v6/channels/{}/messages", channel_id.unwrap()).as_str())
+          .header("authorization", token)
+          .form(&[("content", std::str::from_utf8(&buffer).expect("message not utf-8"))])
+          .send()
+          .await?;
+      }
+    }
   }
-  if let Ok(_) = std::fs::write("config", format!("{}\n{}", server_name, channel_name)) {}
+  if let Ok(_) = std::fs::write("config", format!("{}\n{}", server_name, channel_name)) {
+  }
   Ok(())
 }
 
